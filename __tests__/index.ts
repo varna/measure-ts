6c25b02cf3b08cc214b27b3dbfd1b0ef7b0d84b9
Paulius Varna <paul.varna@gmail.com>
2019-07-03 16:04:35 +0300

    Added new test cases

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -5,3 +5,43 @@ test('basic', () => {
   const res = formatUnit('length')(km_in_m)('km')
   expect(res).toBe('1.000 km')
 })
+
+test('9', () => {
+  const res = formatUnit('length')(9)(['m', 'km'])
+  expect(res).toBe('9 m')
+})
+
+test('99', () => {
+  const res = formatUnit('length')(99)(['m', 'km'])
+  expect(res).toBe('99 m')
+})
+
+test('999', () => {
+  const res = formatUnit('length')(999)(['m', 'km'])
+  expect(res).toBe('999 m')
+})
+
+test('999.9 e2', () => {
+  const res = formatUnit('length')(999.9)(['m', 'km'])
+  expect(res).toBe('999.9 m')
+})
+
+test('999.99 e2', () => {
+  const res = formatUnit('length')(999.99)(['m', 'km'])
+  expect(res).toBe('999.99 m')
+})
+
+test('999.999 e2', () => {
+  const res = formatUnit('length')(999.999)(['m', 'km'])
+  expect(res).toBe('1 km')
+})
+
+test('0.001 e2', () => {
+  const res = formatUnit('length')(0.001)(['m', 'km'])
+  expect(res).toBe('0 m')
+})
+
+test('0.005 e2', () => {
+  const res = formatUnit('length')(0.005)(['m', 'km'])
+  expect(res).toBe('0.01 m')
+})
