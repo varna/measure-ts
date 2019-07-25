ce4241c6cd74ee01d605c552517372b2d178047c
Paulius Varna <paul.varna@gmail.com>
2019-07-25 13:20:38 +0300

    annotated Measure and measure.to

--- src/measure.ts
+++ src/measure.ts
@@ -2,7 +2,13 @@ import findUnit from './findUnit'
 import { UnitKey } from './units'
 
 /**
- * Measure space in meters.
+ * Measure of value with unit.
+ * Length, area, volume and mass units.
+ * SI, metric and imperial units.
+ * Squared and cubed length units (m2, m3, km2, m3, ft3, mi2).
+ * SI prefixes (da, h, k, M, G, T, P, E, Z, Y, d, c, m, μ, n, p, f, a, z, y) for (m, m2, m3, l, g) i.e. (km2, kg, cm, ml).
+ *
+ * @returns {Number|String} `Measure.valueOf(): Number` or `Measure.toString(): String` depending on context
  */
 export default class Measure extends Number implements Number {
   public precision = 12
@@ -13,10 +19,14 @@ export default class Measure extends Number implements Number {
   public suffix = ''
   public round = 2
 
-  constructor(value: number | string | Measure, unitString?: UnitKey) {
+  /**
+   * @param value value/quantity of unit
+   * @param unitKey key representation of unit
+   */
+  constructor(value: number | string | Measure, unitKey?: UnitKey) {
     super(value)
-    if (unitString) {
-      const { unit, prefix, suffix, ratio } = findUnit(unitString)
+    if (unitKey) {
+      const { unit, prefix, suffix, ratio } = findUnit(unitKey)
       this.symbol = unit
       this.prefix = prefix
       this.suffix = suffix
@@ -24,8 +34,13 @@ export default class Measure extends Number implements Number {
     }
   }
 
-  public to = (unitString: UnitKey): Measure => {
-    const { unit, prefix, suffix, ratio } = findUnit(unitString)
+  /**
+   * Convert Measure to another Measure with different Unit.
+   *
+   * @param unitKey key representation of unit
+   */
+  public to = (unitKey: UnitKey): Measure => {
+    const { unit, prefix, suffix, ratio } = findUnit(unitKey)
     this.symbol = unit
     this.prefix = prefix
     this.suffix = suffix
@@ -37,6 +52,12 @@ export default class Measure extends Number implements Number {
     return this.clone(newValue)
   }
 
+  /**
+   * Create a copy of of Measure with new value.
+   * Why? Because value of Number that we extend is immutable.
+   *
+   * @param value new value
+   */
   public clone(value: number) {
     const measure = new Measure(value)
     measure.ratio = this.ratio
